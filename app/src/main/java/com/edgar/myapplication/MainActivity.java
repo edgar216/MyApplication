eeedfa8f6129c56768a970ab7fc0b115b8439959
edgar <liucj1986@yeah.net>
2015-05-04 22:24:18 +0800

    test

--- app/src/main/java/com/edgar/myapplication/MainActivity.java
+++ app/src/main/java/com/edgar/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+    @Override  //fdsaljkl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
